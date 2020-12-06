f3984d5655c92f2802078109ef3ea2e405973b7b
OkazakiYumemi <1079942153@qq.com>
2020-12-06 16:30:36 +0800

    Site updated: 2020-12-06 16:30:24

---
+++ hide/20201101/shuffle.h
@@ -0,0 +1,7 @@
+#ifndef SHUFFLE_H
+#define SHUFFLE_H
+#include<vector>
+using namespace std;
+vector<int> solve(int n, int b, int k, int q, int ST);
+vector<vector<int> > shuffle(vector<vector<int> > boxes);
+#endif
